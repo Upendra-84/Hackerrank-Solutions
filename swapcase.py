d8a3905ba669fd4d478748d04f15e12e6ad14f10
Vardaan Bhalla <64959914+vardaanbhalla@users.noreply.github.com>
2020-06-03 23:05:38 +0530

    sWAP Case - Hackerrank Solution in Py3
    sWAP Case - Hackerrank Solution in Py3

---
+++ swapcase.py
@@ -0,0 +1,13 @@
+def swap_case(k):
+        a = ""
+        for i in k:
+            if i.isupper() == True:
+                a+=(i.lower())
+            else:
+                a+=(i.upper())
+        return a
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
