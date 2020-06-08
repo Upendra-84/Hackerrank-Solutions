a1561798c047d5dab0122eceb815916c8e9893a0
Vardaan Bhalla <64959914+vardaanbhalla@users.noreply.github.com>
2020-06-09 00:44:03 +0530

    String Validators - Hackerrank Solution in Python3
    String Validators - Hackerrank Solution in Python3

---
+++ svalid.py
@@ -0,0 +1,7 @@
+s = input()
+
+print(any([i.isalnum() for i in string]))
+print(any([i.isalpha() for i in string]))
+print(any([i.isdigit() for i in string]))
+print(any([i.islower() for i in string]))
+print(any([i.isupper() for i in string]))
